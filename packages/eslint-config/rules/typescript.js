91c7169ed3d6711d675263025731607e6b4ba223
Will Sabransky <will.sabransky@postmates.com>
2019-01-21 10:28:44 -0800

    Switch to new @typescript-eslint packages

--- packages/eslint-config/rules/typescript.js
+++ packages/eslint-config/rules/typescript.js
@@ -1,16 +1,16 @@
 module.exports = {
   plugins: [
     'jsx-a11y',
-    'typescript',
+    '@typescript-eslint',
   ],
   overrides: [{
-    parser: 'typescript-eslint-parser',
+    parser: '@typescript-eslint/parser',
     files: ['*.ts', '*.tsx'],
     rules: {
       'no-useless-constructor': 'off',
       'no-undef': 'off',
       'no-unused-vars': 'off',
-      'typescript/no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
       'react/prop-types': 'off',
       'no-param-reassign': 'off',
       'space-infix-ops': 'off',
@@ -23,10 +23,10 @@ module.exports = {
       'jsx-a11y/anchor-is-valid': 'off',
 
       'arrow-parens': 'error',
-      'typescript/adjacent-overload-signatures': 'error',
-      'typescript/member-delimiter-style': 'error',
-      'typescript/no-angle-bracket-type-assertion': 'error',
-      'typescript/type-annotation-spacing': 'error',
+      '@typescript-eslint/adjacent-overload-signatures': 'error',
+      '@typescript-eslint/member-delimiter-style': 'error',
+      '@typescript-eslint/no-angle-bracket-type-assertion': 'error',
+      '@typescript-eslint/type-annotation-spacing': 'error',
     }
   }]
 };
